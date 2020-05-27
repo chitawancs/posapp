b516843b18f22c9f9e112c84603ed951b1b23726
Chitawan <chitawan.cs@gmail.com>
2020-05-27 16:39:48 +0700

    Initial commit

---
+++ app/src/main/java/com/easyfo/posapp/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.easyfo.posapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
